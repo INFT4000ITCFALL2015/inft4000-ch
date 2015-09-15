e1da2eaf26ce4a01189fbbc7f19acdea0071d4fc
inet2005 <38High36>
2015-09-15 13:59:56 -0300

    Lab1 - Demo Files - Lab not started yet.

---
+++ Lab1/scripts.js
@@ -0,0 +1,15 @@
+"use strict";
+
+			function sayTwoPlusTwo() 
+			{
+				var total;
+				total = 2 + 2;
+				alert("2 + 2 = " + total);
+			}
+			
+			function sayTwoPlusThree() 
+			{
+				var total;
+				total = 2 + 3;
+				alert("2 + 3 = " + total);
+			}
