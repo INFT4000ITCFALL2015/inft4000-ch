ecb0e245484576a51ed44a1935e48e7ca98bc597
inet2005 <38High36>
2015-10-20 19:43:55 -0300

    Assignment #1 - JS Validation complete

--- Assignment1/js/sudoku.js
+++ Assignment1/js/sudoku.js
@@ -1,3 +1,46 @@
-/**
- * Created by inet2005 on 10/20/15.
- */
+/*jslint browser: true*/
+/*jslint plusplus: true*/
+/*global  $*/
+
+$(document).ready(function () {
+    "use strict";
+
+    var cells = $("td");
+
+    function setEditableCells() {
+        var i;
+        for (i = 0; i < cells.length; i++) {
+            if ($(cells[i]).text() === "") {
+                $(cells[i]).attr('contenteditable', 'true');
+                $(cells[i]).attr('class', 'emptyCell');
+            }
+        }
+    }// end setEditableCells
+
+    function checkInput(editedCell, keyPressed) {
+        var regexPattern = /^[1-9]$/,
+            currentInput = $(editedCell).text(),
+            isValid = true;
+        if (!regexPattern.test(keyPressed)) {
+            isValid = false;
+            // show an error
+        } else if (currentInput.length > 0) {
+            isValid = false;
+        }
+        return isValid;
+    }// end checkInput
+
+    setEditableCells();
+
+    // -----LISTENERS-----
+
+    $(cells).keydown(function (event) {
+        var keyPressed = String.fromCharCode(event.keyCode || event.charCode);
+        // check for DEL or Backspace
+        if (event.keyCode === 8 || event.keyCode === 46) {
+            // do nothing
+        } else if (!checkInput(this, keyPressed)) {
+            event.preventDefault();
+        }
+    });// end cells keydown
+});
